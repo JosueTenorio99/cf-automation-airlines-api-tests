83fa7dec4b3922a6f93fc360284603036364424e
Josué Tenorio <tndjosue69@gmail.com>
2025-07-18 16:41:06 -0600

    Se crearon los fixtures del authroken

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,48 +4,47 @@ import string
 
 import requests, pytest, faker
 from dotenv import load_dotenv
+fake = faker.Faker
 
 load_dotenv()
 BASE_URL = "https://cf-automation-airline-api.onrender.com"
-AUTH_LOGIN = "/auth/login/"
-AIRPORT = "/airports/"
+AUTH_LOGIN = "/auth/login"
+AIRPORT = "/airports"
+USERS = "/users"
 fake = faker.Faker()
 
 @pytest.fixture(scope="session")
-def admin_token() -> str:
+def admin_token():
     user = os.getenv("ADMIN_USER")
-    pwd = os.getenv("ADMIN_PASS")
-
+    pwd = os.getenv("ADMIN_PASSWORD")
     r = requests.post(BASE_URL + AUTH_LOGIN,
                       data={"username": user, "password": pwd},
                       timeout=5)
-
     r.raise_for_status()
     return r.json()["access_token"]
-
 
 @pytest.fixture
 def auth_headers(admin_token):
     return {"Authorization": f"Bearer {admin_token}"}
 
-
 @pytest.fixture
 def airport(auth_headers):
-    aiport_data = {
-      "iata_code": "".join(random.choices(string.ascii_uppercase, k=3)),
-      "city": "La Paz",
-      "country": fake.country_code()
+    airport_data= {
+        "iata_code": "".join(random.choices(string.ascii_uppercase, k=3)),
+        "city": "La paz",
+        "country": fake.country_code()
     }
 
-    r = requests.post(BASE_URL + AIRPORT, json=aiport_data, headers=auth_headers, timeout=5)
+    r = requests.post(BASE_URL + AIRPORT,
+                      json=airport_data,
+                      headers=auth_headers,
+                      timeout=5)
     r.raise_for_status()
     airport_response = r.json()
     yield airport_response
-    requests.delete(BASE_URL + AIRPORT + f'{airport_response["iata_code"]}', headers=auth_headers, timeout=5)
-
-
-def test_admin_token(admin_token):
-    return admin_token
+    requests.delete(f"{BASE_URL}{AIRPORT}{airport_response["iata_code"]}",
+                    headers=auth_headers,
+                    timeout=5)
 
 def test_airport(airport):
-    print(airport)
+    return airport
